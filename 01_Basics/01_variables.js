a58b5fbb117cbea845925950cc78f897cf719b5f
varurohit <160455966+varurohit@users.noreply.github.com>
2024-02-19 05:25:06 +0000

    js basic var and datatypes

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,11 @@
+var rohitId = "rohit"
+let rs="dsd"
+const Id=12
+r="23"
+
+/*
+var => datatype not use because block and functionality issues
+prefar let 
+ */
+
+console.table([rohitId,r,Id,rs])
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict";//treat all js code new version js code
+// alert("hello") //we are useing node.js not browser
+ let int=0
+ let str=""
+ let f=null
+ let empy;
+  
+ //datatype in js
+ // int=> numbers 2 power 53
+ //bigint
+ //string=>"weww"
+ //boolean=>true/false
+ //undefined
+ //null=>standalone value consider as object
+ //symbol=>unique
+
+ //object =>
+
+ console.log(typeof f)
