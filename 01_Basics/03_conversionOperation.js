c71f84b4b86c5a3651d09ace9bf927315d17c9ab
R.V. <160455966+varurohit@users.noreply.github.com>
2024-02-23 18:18:07 +0000

    conversion of data types

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = null
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNum = Number(score)
+
+// console.log(typeof valueInNum);
+// console.log(valueInNum);
+
+// 33 => number
+// "33aba" => NaN
+// true => 1; false => 0
+// null => 0
+
+let isLogin = "rohit"
+let boolVar = Boolean(isLogin)
+
+//console.log(boolVar);
+
+// 1 => true
+// 0 => false
+// ""=> false
+//"rohit" => true
+
+let someNumber = 33
+let str = String(someNumber)
+
+console.log(str);
+console.log(typeof str);
